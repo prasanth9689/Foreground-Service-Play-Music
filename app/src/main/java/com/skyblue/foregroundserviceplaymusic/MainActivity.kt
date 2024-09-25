4221e0ff4ace917b4fd8b18e8c6422fb9e7003dd
Prasanth <prasanthcena968@gmail.com>
2024-09-25 17:55:42 +0530

    updated

--- app/src/main/java/com/skyblue/foregroundserviceplaymusic/MainActivity.kt
+++ app/src/main/java/com/skyblue/foregroundserviceplaymusic/MainActivity.kt
@@ -1,66 +1,131 @@
 package com.skyblue.foregroundserviceplaymusic
 
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
+import android.Manifest
+import android.content.ContentUris
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.skyblue.foregroundserviceplaymusic.databinding.ActivityMainBinding
+import java.util.LinkedList
+import java.util.SortedSet
+import java.util.TreeSet
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var context = this@MainActivity
     private val TAG = "MainActivity_"
-    private var musicService: MusicPlayerService? = null
-    private var isBound = false
+    private var allLoaded = false
+    private var rowsToLoad = 0
+    private var startingRow = 0
 
-    private val connection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as MusicPlayerService.MusicBinder
-            musicService = binder.getService()
-            isBound = true
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            isBound = false
-        }
-    }
-
+    /*
+     https://medium.com/android-news/custom-gallery-for-android-af2437b227da
+     https://github.com/mmobin789/Android-Custom-Gallery/blob/master/app-rx/src/main/java/rx/kt/gallery/viewmodel/GalleryViewModel.kt#L29
+     */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        requestReadStoragePermission()
+        loadMusicList()
+
         binding.start.setOnClickListener {
-            startMusic()
-        }
-        binding.stop.setOnClickListener {
-            stopMusic()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, MusicPlayerService::class.java)
-        bindService(intent, connection, Context.BIND_AUTO_CREATE)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (isBound) {
-            unbindService(connection)
-            isBound = false
+            val intent = Intent(context, MusicPlayerService::class.java)
+            intent.putExtra("music_name", "Kattre  enthan geetham")
+            startService(intent)
         }
     }
 
-    fun startMusic() {
-        musicService?.startMusic()
+    private fun requestReadStoragePermission() {
+        val readStorage = Manifest.permission.READ_EXTERNAL_STORAGE
+        if (ContextCompat.checkSelfPermission(
+                this,
+                readStorage
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(readStorage), 3)
+        } else initNow()
     }
 
-    fun stopMusic() {
-        musicService?.pauseMusic()
+    private fun initNow() {
+        Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun fetchGalleryImages(context: Context, rowsPerLoad: Int): List<GalleryPicture> {
+        val galleryImageUrls = LinkedList<GalleryPicture>()
+        val cursor = getGalleryCursor(context)
+
+        if (cursor != null && !allLoaded) {
+            val totalRows = cursor.count
+
+            allLoaded = rowsToLoad == totalRows
+            if (rowsToLoad < rowsPerLoad) {
+                rowsToLoad = rowsPerLoad
+            }
+
+            for (i in startingRow until rowsToLoad) {
+                cursor.moveToPosition(i)
+                val dataColumnIndex =
+                    cursor.getColumnIndex(MediaStore.MediaColumns._ID) //get column index
+                galleryImageUrls.add(GalleryPicture(getImageUri(cursor.getString(dataColumnIndex)).toString())) //get Image path from column index
+
+            }
+            Log.i(TAG, "TotalGallerySize : $totalRows")
+            Log.i(TAG, "GalleryStart : $startingRow")
+            Log.i(TAG , "GalleryEnd : $rowsToLoad")
+
+            startingRow = rowsToLoad
+
+            if (rowsPerLoad > totalRows || rowsToLoad >= totalRows)
+                rowsToLoad = totalRows
+            else {
+                if (totalRows - rowsToLoad <= rowsPerLoad)
+                    rowsToLoad = totalRows
+                else
+                    rowsToLoad += rowsPerLoad
+            }
+
+            cursor.close()
+            Log.i(TAG, "PartialGallerySize ${galleryImageUrls.size}")
+        }
+        return galleryImageUrls
+    }
+
+    private fun getGalleryCursor(context: Context): Cursor? {
+        val externalUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val columns = arrayOf(MediaStore.MediaColumns._ID, MediaStore.MediaColumns.DATE_MODIFIED)
+        val orderBy = MediaStore.MediaColumns.DATE_MODIFIED //order data by modified
+        return context.contentResolver
+            .query(
+                externalUri,
+                columns,
+                null,
+                null,
+                "$orderBy DESC"
+            )//get all data in Cursor by sorting in DESC order
+    }
+
+    data class GalleryPicture(val path: String) {
+        var isSelected = false
+    }
+
+    private fun getImageUri(path: String) = ContentUris.withAppendedId(
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        path.toLong()
+    )
+
+    private fun loadMusicList() {
+        fetchGalleryImages(context, 5)
     }
 }
--- app/src/main/java/com/skyblue/foregroundserviceplaymusic/MusicPlayerService.kt
+++ app/src/main/java/com/skyblue/foregroundserviceplaymusic/MusicPlayerService.kt
@@ -1,42 +1,72 @@
 package com.skyblue.foregroundserviceplaymusic
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
-import android.media.MediaPlayer
-import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MusicPlayerService: Service() {
-    private lateinit var mediaPlayer: MediaPlayer
-    private val binder = MusicBinder()
+    private val TAG = "MainActivity_"
 
-    inner class MusicBinder : Binder() {
-        fun getService(): MusicPlayerService = this@MusicPlayerService
-    }
-
+    @Override
     override fun onBind(intent: Intent?): IBinder? {
-        return binder
+        return null
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        mediaPlayer = MediaPlayer.create(this, R.raw.tom_and_jerry_theme)
-        mediaPlayer.isLooping = true
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val songName = intent?.getStringExtra("music_name")
+
+        // Service has started running in the background
+        if (intent != null){
+            Log.d(TAG, "Service Name: $songName")
+        }
+
+        // Service Status : Service has been started
+        for (i in 1..10){
+            Thread.sleep(100)
+            Log.d(TAG, "Service is running: $i")
+        }
+        createNotification(songName);
+
+        stopSelf()
+        return START_STICKY
     }
 
-    fun startMusic(){
-        if (!mediaPlayer.isPlaying){
-            mediaPlayer.start()
+    private fun createNotification(songName: String?) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            val channel = NotificationChannel(
+                "running_channel",
+                "Running Notifications",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val notificationManager= getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
+
+        val notification = NotificationCompat.Builder(this, "running_channel")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Run is active")
+            .setContentText(songName)
+            .build()
+        startForeground(1234, notification)
     }
-    fun pauseMusic(){
-        if (mediaPlayer.isPlaying){
-            mediaPlayer.pause()
-        }
+
+    override fun stopService(name: Intent?): Boolean {
+        // Service Status : Service has been stopped
+        return super.stopService(name)
     }
 
     override fun onDestroy() {
+        // Service execution completed
+        Log.d(TAG,"Stopped : Service Stopped")
         super.onDestroy()
-        mediaPlayer.release()
     }
+
 }
